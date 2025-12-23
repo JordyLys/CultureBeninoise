55705eb14ded5c8d22fd0dc973a0a5716b270756
JordyLys <jordyatchaoue@gmail.com>
2025-12-23 12:37:00 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+echo "🚀 Starting Laravel container..."
+
 PORT_INT=$(php -r 'echo (int) getenv("PORT");')
 
-echo "Starting Laravel on port $PORT_INT"
+echo "📡 Using port $PORT_INT"
 
+# Attendre que la DB soit prête (important)
+sleep 5
+
+# Lancer migrations si nécessaire
+php artisan migrate --force || true
+
+# Lancer le serveur
 php artisan serve --host=0.0.0.0 --port=$PORT_INT
