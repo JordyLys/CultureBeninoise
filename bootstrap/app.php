a70cdc7be3df96e31b063ef7653bcc4c44e6ec0f
JordyLys <jordyatchaoue@gmail.com>
2025-12-23 11:27:47 +0100

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,22 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\Admin;
 
-// Créer l'application Laravel
-$app = new Application(dirname(__DIR__));
 
-// Retourner l'application
-return $app;
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias([
+            'admin'=>Admin::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
