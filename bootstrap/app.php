0d85c4e5719a8a910c5bb5aeb22fc6b131fbfd97
JordyLys <jordyatchaoue@gmail.com>
2025-12-23 11:14:18 +0100

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,47 +2,9 @@
 
 use Illuminate\Foundation\Application;
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| Ici, nous créons l’instance de l’application Laravel.
-|
-*/
-
 $app = new Application(
     base_path: dirname(__DIR__)
 );
 
-/*
-|--------------------------------------------------------------------------
-| Bind Important Interfaces
-|--------------------------------------------------------------------------
-|
-| Lier les interfaces essentielles pour le framework.
-|
-*/
-
-$app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
-);
-
-/*
-|--------------------------------------------------------------------------
-| Retourner l’application
-|--------------------------------------------------------------------------
-*/
-
+// Retourner l'application
 return $app;
